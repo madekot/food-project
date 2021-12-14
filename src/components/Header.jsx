72e492519e2afef850a88c7d241a222f8b2642b3
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-12-14 23:14:49 +0300

    84. Компонент Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a href="#!" className="brand-logo">
+        <Link to="#!" className="brand-logo">
           Shop
-        </a>
+        </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="sass.html">React Shop</a>
+            <Link to="sass.html">React Shop</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
           </li>
         </ul>
       </div>
