51e39f90d3beb04ffbfc876c7861d7768a140b5a
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-12-17 23:44:27 +0300

    90. Главная страница с категориями

--- src/components/Category/Item.jsx
+++ src/components/Category/Item.jsx
@@ -1,7 +1,29 @@
+import { Link } from 'react-router-dom';
+
 function Item(props) {
-  console.log(props);
-  const { name } = props;
-  return <li className="card">{name}</li>;
+  const {
+    // idCategory,
+    name,
+    image,
+    description,
+  } = props;
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={image} alt={name} />
+      </div>
+      <div className="card-content">
+        <span className="card-title">{name}</span>
+        <p>{description.slice(0, 60)}...</p>
+      </div>
+      <div className="card-action">
+        <Link to={`/category/${name}`} className="btn">
+          Watch category
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Item;
