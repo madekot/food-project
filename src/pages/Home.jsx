bdd5809800443af760bae78909363e382a3191d4
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-12-24 21:52:39 +0300

    work and progress, 93. Поиск на главной

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,40 @@ import { useState, useEffect } from 'react';
 import { getAllCategories } from '../api';
 import Preloader from '../components/Preloader';
 import { List as CategoryList } from '../components/Category';
+import Search from '../components/Search';
 
 function Home() {
   const [catalog, setCatalog] = useState([]);
   const isCatalogEmpty = catalog.length === 0;
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.category.toLowerCase().includes(str.toLowerCase()),
+      ),
+    );
+  };
 
   const content = isCatalogEmpty ? (
     <Preloader />
   ) : (
-    <CategoryList catalog={catalog} />
+    <CategoryList catalog={setFilteredCatalog} />
   );
 
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data);
+      setFilteredCatalog(data);
     });
   }, []);
 
-  return content;
+  return (
+    <>
+      <Search cb={() => {}} />
+      {content}
+    </>
+  );
 }
 
 export default Home;
