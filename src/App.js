a8d0aae99ae336bdef1cda913c4652bd982869cd
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-12-25 11:28:11 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Recipe from './pages/Recipe';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/food-project">
         <Header />
         <main className="container content">
           <Switch>
